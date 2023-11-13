dc88a04bae4fd31ad0bdaa3faa054acac3763782
DanielJDufour <daniel.j.dufour@gmail.com>
2023-11-12 20:06:40 -0800

    mvp hopefully

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 - Mixed Projections
 - Turbo-Charging Reprojection
 - Web Workers
+- Simple SRS (inspired by [CRS.Simple](https://leafletjs.com/examples/crs-simple/crs-simple.html))
 
 ### Known Limitations
 - Only works in the Browser
@@ -23,32 +24,62 @@ import { GeoRasterReader } from "georaster-reader";
 
 import parseGeoRaster from "georaster";
 
-const georaster = await parseGeoraster("https://example.org/wildfires.tiff");
+const reader = new GeoRasterReader({
+  // multiple sources
+  // they can even be in different projections!
+  sources: [
+     await parseGeoraster("https://example.org/red.tiff"),
+     await parseGeoraster("https://example.org/green.tiff")
+     await parseGeoraster("https://example.org/blue.tiff")
+  ]
+});
 
-const reader = new GeoRasterReader({ sources: [georaster] });
-
-reader.read({
+const result = await reader.read({
   // instance of GeoExtent (https://www.npmjs.com/package/geo-extent)
   // an extent is a bbox with a spatial reference system
   extent: new GeoExtent([-122.49755859375, 38.8520508, -120.06958007812499, 40.697299008636755], { srs: 4326 }),
   size: [width, height]
 });
+
+// result is the following object
+{
+  // multi-dimensional numberical arrays separated by source, then band, then row, then column
+  data: [
+    [
+      [
+        [6453, 7692, 12722, 7274, 6302, 6234, 6147, 6276, ...], // first row of first band
+        [5997, 9353, 12493, 6389, 10166, 6285, 10634, 7954, ...] // second row of second band
+      ],
+      [ ... ] // second band
+      [ ... ] // third band
+    ],
+    [ [ [...] ... ] ... ] // second georaster
+    [ [ [...] ... ] ... ] // third georaster
+  ],
+  size: [ result_data_width, result_data_height ]
+}
 ```
 
 ### advanced usage
+#### optional properties
 ```js
 const reader = new GeoRasterReader({
-  // multiple sources
-  // they can even be in different projections!
-  sources: [
-     await parseGeoraster("https://example.org/wildfires_red.tiff"),
-     await parseGeoraster("https://example.org/wildfires_green.tiff")
-     await parseGeoraster("https://example.org/blue.tiff")
-  ],
+  sources: [...],
 
   // optional properties
   debugLevel,
+  flat: true, // flatten all the georaster results when reading by one level, so they appear as if they came from the same source
   method: "near-vectorize", // default resampling method via https://github.com/danieljdufour/geowarp
   turbo: true // apply experimential projection turbo charging via https://github.com/DanielJDufour/proj-turbo
+});
+```
+
+### simple projection
+If you want to read using pixel coordinates, pass in an extent with srs set to "simple".
+Inspired by Leaflet's Simple CRS, "simple" srs is when the bottom left corner of the image is [0, 0)]
+and the top-right of the image is [image_width, image_height].
+```js
+await reader.read({
+  extent: new GeoExtent([512, 256, 768, 512], { srs: "simple" }),
 });
 ```
--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const chooseAtRandom = arr => arr[Math.floor(Math.random() * arr.length)];
 const range = ct => new Array(ct).fill(0).map((_, i) => i);
 
 class GeoRasterReader {
-  constructor({ debugLevel, method, sources, turbo }) {
+  constructor({ debugLevel, flat = false, method, sources, turbo }) {
     this.debugLevel = debugLevel;
+    this.flat = flat || false;
     this.georasters = sources;
     this.defaultMethod = method;
     this.defaultTurbo = turbo;
@@ -21,55 +22,83 @@ class GeoRasterReader {
     const [width, height] = size;
 
     const out_array_types = ["Array", "Array", "Array"];
-    const out_bbox = extent.bbox;
-    const out_srs = Number(extent.srs.replace("EPSG:", ""));
+
     const resample_method = this.defaultMethod || "near-vectorize";
 
     const promises = this.georasters.map(async georaster => {
       // old-school in-memory georaster from an ArrayBuffer or simple object
       if (georaster.values) {
-        const { forward, inverse } = proj4fullyloaded("EPSG:" + georaster.projection, extent.srs);
+        if (extent.srs === "simple") {
+          return geowarp({
+            debug_level: Math.max((this.debugLevel || 3) - 3, 0),
+            in_data: georaster.values,
+            in_bbox: [0, 0, georaster.width, georaster.height],
+            in_layout: "[band][row][column]",
+            in_width: georaster.width,
+            in_height: georaster.height,
+            out_array_types,
+            out_bbox: extent.bbox,
+            out_layout: "[band][row][column]",
+            out_height: height,
+            out_width: width,
+            method: resample_method,
+            round: false,
+            turbo: this.turbo
+          }).data;
+        } else {
+          const { forward, inverse } = proj4fullyloaded("EPSG:" + georaster.projection, extent.srs);
 
-        return geowarp({
-          debug_level: Math.max((this.debugLevel || 3) - 3, 0),
-          forward,
-          inverse,
-          in_data: georaster.values,
-          in_bbox: [georaster.xmin, georaster.ymin, georaster.xmax, georaster.ymax],
-          in_layout: "[band][row][column]",
-          in_srs: georaster.projection,
-          in_width: georaster.width,
-          in_height: georaster.height,
-          out_array_types,
-          out_bbox,
-          out_layout: "[band][row][column]",
-          out_srs,
-          out_height: height,
-          out_width: width,
-          method: resample_method,
-          round: false,
-          turbo: this.turbo
-        }).data;
+          return geowarp({
+            debug_level: Math.max((this.debugLevel || 3) - 3, 0),
+            forward,
+            inverse,
+            in_data: georaster.values,
+            in_bbox: [georaster.xmin, georaster.ymin, georaster.xmax, georaster.ymax],
+            in_layout: "[band][row][column]",
+            in_srs: georaster.projection,
+            in_width: georaster.width,
+            in_height: georaster.height,
+            out_array_types,
+            out_bbox: extent.bbox,
+            out_layout: "[band][row][column]",
+            out_srs: extent.srs,
+            out_height: height,
+            out_width: width,
+            method: resample_method,
+            round: false,
+            turbo: this.turbo
+          }).data;
+        }
       } else {
         const baseReadTileParams = {
-          bbox: out_bbox,
-          bbox_srs: out_srs,
+          bbox: extent.bbox,
+          bbox_srs: extent.srs,
           debug_level: 5,
           method: resample_method,
           round: false,
           tile_array_types: out_array_types,
           tile_height: height,
           tile_layout: "[band][row][column]",
-          tile_srs: out_srs,
+          tile_srs: extent.srs,
           tile_width: width,
           timed: true,
           use_overview: true,
           turbo: this.turbo
         };
+
+        if (extent.srs === "simple") {
+          // this will prevent attempts at resampling because
+          // the input (geotiff) and output srs will be treated as the same
+          baseReadTileParams.geotiff_srs = "simple";
+        }
+
         if (this.debugLevel >= 2) console.log("[georaster-reader] baseReadTileParams:", baseReadTileParams);
         if (georaster._url) {
           const tileWorker = await chooseAtRandom(this.tileWorkers);
-          return tileWorker.createTile({ ...baseReadTileParams, url: georaster._url, debug_level: 4 }).then(({ tile }) => tile);
+          const createTileParams = { ...baseReadTileParams, url: georaster._url, debug_level: 4 };
+          if (this.debugLevel >= 1) console.log("[georaster-reader] calling createTile with:", createTileParams);
+          const created = await tileWorker.createTile(createTileParams);
+          return created.tile;
         } else if (georaster._geotiff) {
           return readTile({ ...baseReadTileParams, geotiff: georaster._geotiff }).then(({ tile }) => tile);
         }
@@ -80,9 +109,9 @@ class GeoRasterReader {
     if (this.debugLevel >= 4) console.log("[georaster-reader] valuesByGeoRaster:", valuesByGeoRaster);
 
     // flatten the stack
-    const result = valuesByGeoRaster.flat();
+    const result = this.flat ? valuesByGeoRaster.flat() : valuesByGeoRaster;
     if (this.debugLevel >= 1) console.log("[georaster-reader] read rasters took:", performance.now() - startReadRasters, "ms");
-    return result;
+    return { data: result, size };
   }
 }
 
